fa34b1c39aa3fdb4720c852ef833a995bc64b7da
Pawel <office.wand.studio@gmail.com>
2024-02-27 21:06:04 +0100

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,8 +39,8 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
 /* show scroll up */
 const scrollUp = () =>{
 	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    // When the scroll is higher than 150 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 150 ? scrollUp.classList.add('show-scroll')
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
